92c1082a659c2ccb5813b9cfa867a91fdb4b5ade
unknown <mohaaouragh2004@gmail.com>
2025-09-22 19:39:36 +0200

    lliurament exercici 3

---
+++ exercici3.java
@@ -0,0 +1,35 @@
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.util.Random;
+
+public class exercici3 {
+
+    public static void main(String[] args) {
+        String archivo = "secret.bin";
+        Random random = new Random();
+
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(archivo))) {
+            int numRandom = random.nextInt(3) + 1;
+
+            for (int i = 0; i < 10; i++) {
+                dos.writeInt(numRandom);
+
+                StringBuilder secret = new StringBuilder();
+                for (int j = 0; j < 3; j++) {
+                    char word = (char) ('a' + random.nextInt(26));
+                    dos.writeChar(word);
+                    secret.append(word);
+                }
+
+                System.out.println(numRandom + ":" + secret);
+
+                numRandom += random.nextInt(3) + 1;
+            }
+
+            System.out.println("Archivo generado correctamente: " + archivo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
